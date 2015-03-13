1ef3ff5547e4bc86fc0b7609df75c4e76d2fcb78
t-j-larsen <tim.larsen.job@gmail.com>
2015-03-13 08:29:15 +0000

    Add test cases without implementations

--- JavaProblem/src/test/java/SuperMarketPlusPlusTest.java
+++ JavaProblem/src/test/java/SuperMarketPlusPlusTest.java
@@ -1,3 +1,4 @@
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -6,7 +7,47 @@ import org.junit.Test;
 public class SuperMarketPlusPlusTest {
 
 	@Test
-	public void testTheTruth() {
+	public void testSellInAndQualityUpdated() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testQualityDegradesTwiceAsFast() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testQualityDegradesTwiceAsFast() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testQualityNeverNegative() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testQualityNotMoreThan50ExceptSulfuras() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testIncreaseInQuality() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testSulfuras() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testBackstagePasses() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testOrganicBananas() {
 		assertTrue(true);
 	}
 }
