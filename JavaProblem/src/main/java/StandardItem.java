9eae222273b2e7edf111269e447fc0e9312b8e52
t-j-larsen <tim.larsen.job@gmail.com>
2015-03-13 14:33:48 +0000

    Add maxQuality functionality and tests

--- JavaProblem/src/main/java/StandardItem.java
+++ JavaProblem/src/main/java/StandardItem.java
@@ -3,23 +3,43 @@
 
 public class StandardItem extends Item {
 
+	public static final int DEFAUL_MAX_QUALITY = 50;
+	
+	private int maxQuality = DEFAUL_MAX_QUALITY;
+	
 	public StandardItem(String name, int sellIn, int quality) {
-		super(name, sellIn, quality);
+		this(name, sellIn, quality, DEFAUL_MAX_QUALITY);
 	}
 
+	public StandardItem(String name, int sellIn, int quality, int maxQuality) {
+		super(name, sellIn, quality);
+		this.maxQuality = maxQuality;
+		setQuality(quality); // check quality less than maxQuality
+	}
+	
 	public void updateQuality() {
 		sellIn = sellIn - 1;
 		if (quality > 0) {
 			// TODO: confirm exactly the behaviour when sellIn = 0
 			if (sellIn > 0) {
-				quality = quality - 1;	
+				setQuality(quality - 1);	
 			}
 			else {
 				if (quality > 1) {
-					quality = quality - 2;
+					setQuality(quality = quality - 2);
 				}
 			}
 		}
 		
+	}
+	
+	@Override
+	public void setQuality(int quality) {
+		if (quality <= maxQuality) {
+			this.quality = quality;				
+		}
+		else {
+			this.quality = maxQuality; // TODO: confirm requirements
+		}
 	}
 }
--- JavaProblem/src/test/java/ItemTest.java
+++ JavaProblem/src/test/java/ItemTest.java
@@ -8,19 +8,19 @@ public class ItemTest {
 
 	@Test
 	public void testSellInAndQualityUpdated() {
-		StandardItem item = new StandardItem("standard item", 7, 100);
+		StandardItem item = new StandardItem("standard item", 7, 50);
 		item.updateQuality();
-		Assert.assertEquals(99, item.getQuality());
+		Assert.assertEquals(49, item.getQuality());
 		Assert.assertEquals(6, item.getSellIn());
 	}
 	
 	@Test
 	public void testQualityDegradesTwiceAsFast() {
-		StandardItem item = new StandardItem("standard item", 2, 100);
+		StandardItem item = new StandardItem("standard item", 2, 50);
 		item.updateQuality();
-		Assert.assertEquals(99, item.getQuality());
+		Assert.assertEquals(49, item.getQuality());
 		item.updateQuality();
-		Assert.assertEquals(97, item.getQuality());
+		Assert.assertEquals(47, item.getQuality());
 	}
 	
 	@Test
@@ -33,8 +33,19 @@ public class ItemTest {
 	}
 	
 	@Test
-	public void testQualityNotMoreThan50ExceptSulfuras() {
-		assertTrue(true);
+	public void testQualityNotMoreThanDefaultMax() {
+		StandardItem item = new StandardItem("standard item", 1, 51);
+		Assert.assertEquals(StandardItem.DEFAUL_MAX_QUALITY, item.getQuality());
+		item.setQuality(52);
+		Assert.assertEquals(StandardItem.DEFAUL_MAX_QUALITY, item.getQuality());
+	}
+	
+	@Test
+	public void testQualityNotMoreThanCustomerMax() {
+		StandardItem item = new StandardItem("standard item", 1, 51, 80);
+		Assert.assertEquals(51, item.getQuality());
+		item.setQuality(81);
+		Assert.assertEquals(80, item.getQuality());
 	}
 	
 	@Test
